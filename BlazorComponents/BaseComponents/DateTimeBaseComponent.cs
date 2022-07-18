ba12bd6af9cf866a3d3b89488b0f1407b95c96a1
iamstan13y <iamstan13y@gmail.com>
2022-07-18 15:36:40 +0200

    Component inheritance.

---
+++ BlazorComponents/BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponents.BaseComponents
+{
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime DateTime { get; set; }
+    }
+}
